fc3596e93565810ba6e6e5812b5be02ba57d9258
nahedben <59153046+nahedben@users.noreply.github.com>
2022-04-03 11:12:34 +0300

    Update exp1.py

--- exp1.py
+++ exp1.py
@@ -2,6 +2,7 @@ list1 = [x for x in range(5)]
 list2 = list(map(lambda x :x**2 , list1))
 print(list2)
 
+// the second map test powering all items inside list2
 for x in map(lambda x : x *x , list2):
   print(x, end=' ')
 print()
